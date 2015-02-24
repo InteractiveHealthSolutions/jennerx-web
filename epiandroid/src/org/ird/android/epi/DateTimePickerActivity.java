4cc4f60b738888ef563cfe1ae2fc905bcf3bb7b5
saad.qureshi <saad.qureshi@ihsinformatics.com>
2015-02-24 15:07:51 +0500

    Format related changes.

--- epiandroid/src/org/ird/android/epi/DateTimePickerActivity.java
+++ epiandroid/src/org/ird/android/epi/DateTimePickerActivity.java
@@ -17,53 +17,52 @@ import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-public class DateTimePickerActivity extends Activity {
+public class DateTimePickerActivity extends Activity
+{
 
 	private Button btnTime;
 	private Button btnDate;
 	private TextView txtView;
-	DateFormat formatDateTime= DateFormat.getDateTimeInstance();
+	DateFormat formatDateTime = DateFormat.getDateTimeInstance();
 	Calendar cal = Calendar.getInstance();
-	
-		
+
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.date_picker_layout);
-		
-		
-		//btnTime = (Button)findViewById(R.id.btnTime);
-		btnDate = (Button)findViewById(R.id.btnDate);
-		txtView =(TextView) findViewById(R.id.txtViewtime);
-		updateLabel();
 
+		// btnTime = (Button)findViewById(R.id.btnTime);
+		btnDate = (Button) findViewById(R.id.btnDate);
+		txtView = (TextView) findViewById(R.id.txtViewtime);
+		updateLabel();
 	}
-	
+
 	public void chooseDate(View widget)
 	{
 		new DatePickerDialog(DateTimePickerActivity.this,
-				d, 
+				d,
 				cal.get(Calendar.YEAR),
-				cal.get(Calendar.MONTH), 
+				cal.get(Calendar.MONTH),
 				cal.get(Calendar.DAY_OF_MONTH)).show();
 	}
-	
-	DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener()
-	{		
 
+	DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener()
+	{
 		@Override
-		public void onDateSet(DatePicker view, int year, int monthOfYear,
-				int dayOfMonth) {
-			cal.set(Calendar.YEAR,year);
+		public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+		{
+			cal.set(Calendar.YEAR, year);
 			cal.set(Calendar.MONTH, monthOfYear);
 			cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-			updateLabel();			
+			updateLabel();
 		}
 	};
-	
+
 	TimePickerDialog.OnTimeSetListener t = new TimePickerDialog.OnTimeSetListener()
 	{
-		@Override 
+		@Override
 		public void onTimeSet(TimePicker view, int hourOfDay, int minute)
 		{
 			cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
@@ -73,13 +72,15 @@ public class DateTimePickerActivity extends Activity {
 	};
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.date_time_picker, menu);
 		return true;
 	}
-	
-	private void updateLabel() {
+
+	private void updateLabel()
+	{
 		txtView.setText(formatDateTime.format(cal.getTime()));
 	}
 
