888a55d45b1e873f61a65a605060658eac70a16d
saad.qureshi <saad.qureshi@ihsinformatics.com>
2015-03-31 15:29:32 +0500

    1) INVALID code status renamed to appropiate name. 2) Removal of unused imports and commented code.

--- epiandroid/src/org/ird/android/epi/communication/elements/ResponseStatus.java
+++ epiandroid/src/org/ird/android/epi/communication/elements/ResponseStatus.java
@@ -1,29 +1,14 @@
 package org.ird.android.epi.communication.elements;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-import android.R;
-
 public enum ResponseStatus
 {
-	// public static final int INVALID = 0;
-	// public static final int STATUS_SUCCESS= 1;
-	// public static final int STATUS_FAILURE= 2;
-	// public static final int STATUS_DATA_ERROR= 3;
-	// public static final int STATUS_INTERNAL_ERROR= 4;
-	// public static final int STATUS_INCORRECT_DATA_FORMAT_ERROR= 5;
-	// public static final int STATUS_UNRECOGNISED_VERSION= 6;
-	// public static final int STATUS_INCORRECT_CREDENTIALS= 7;
-
 	/*
 	 * Following are errors related to DB and server
 	 * so their status code will be
 	 * more relevant as status message
 	 */
 
-	INVALID(0, "Child ID does not exist", ""),
+	STATUS_ID_CHILD_NOT_EXIST(0, "Child ID does not exist", ""),
 	STATUS_SUCCESS(1, "1", ""),
 	STATUS_FAILURE(2, "2", ""),
 	STATUS_DATA_ERROR(3, "3", ""),
