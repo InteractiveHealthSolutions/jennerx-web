f06e2e224e48c3b20b7a97e43abc84958601280f
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-12-09 09:09:33 +0500

    MK: incentive data export query correction

--- unfepi/src/org/ird/unfepi/queries/ExportChildIncentiveQuery.sql
+++ unfepi/src/org/ird/unfepi/queries/ExportChildIncentiveQuery.sql
@@ -1,41 +1,33 @@
-SELECT i.identifier childid, 
-CONCAT(IFNULL(ch.firstname,''),' ',IFNULL(ch.middlename,''),' ',IFNULL(ch.lastname,'')) nameofchild, 
-CONCAT(IFNULL(ch.fatherFirstname,''),' ',IFNULL(ch.fatherMiddlename,''),' ',IFNULL(ch.fatherLastname,'')) fathernameofchild, 
-ch.enrollmentVaccineId, 
+SELECT 'ProgramID','Name','FatherName','EnrollmentVaccine','ContactNumbers',
+'Addresses','Vaccine','VaccinationRecordNum','VaccinationDuedate','VaccinationDate','VaccinatorID',	
+'CenterID','IncentiveDate','IncentiveStatus','ConsumptionDate','TransactionDate','Amount'
+UNION
+SELECT i.identifier programId, 
+CONCAT(IFNULL(ch.firstname,''),' ',IFNULL(ch.middlename,''),' ',IFNULL(ch.lastname,'')) name, 
+CONCAT(IFNULL(ch.fatherFirstname,''),' ',IFNULL(ch.fatherMiddlename,''),' ',IFNULL(ch.fatherLastname,'')) fatherName, 
 chvc.name enrollmentVaccine, 
-(select group_concat('--',numberType,':',number) from contactnumber where mappedid=vtn.childid) contactnumbersofchild, 
+(select group_concat('--',numberType,':',number) from contactnumber where mappedid=vtn.childid) contactnumbers, 
 (select group_concat('H.No:',IFNULL(a.addHouseNumber,'NIL'),' Street:',IFNULL(a.addStreet,'NIL'),
 ' Sector:',IFNULL(a.addSector,'NIL'),' Colony:',IFNULL(a.addColony,'NIL'),' Town:',IFNULL(a.addtown,'NIL'),
 ' UC:',IFNULL(a.addUc,'NIL'),' LMARK:',IFNULL(a.addLandmark,'NIL'),
-' CityID:',CAST(IFNULL(a.cityId,'') AS char(2))) from address a where a.mappedid=vtn.childid) addressofchild, 
-vtn.vaccineid, 
-vc.name, 
-(case when ch.enrollmentVaccineId=vtn.vaccineId AND vtn.vaccinationStatus='VACCINATED' AND vtn.isFirstVaccination
-		then 'ENROLLMENT' 
-		when ch.enrollmentVaccineId <> vtn.vaccineId AND NOT vtn.isFirstVaccination
-		then 'FOLLOWUP' Else 'NOT_FOUND' end) as 'EncounterOrFormType' , 
+' CityID:',CAST(IFNULL(a.cityId,'') AS char(2))) from address a where a.mappedid=vtn.childid) addresses, 
+vc.name vaccineName, 
 vtn.vaccinationRecordNum,
 vtn.vaccinationDuedate, 
 vtn.vaccinationDate, 
-vctor.identifier AS VaccinatorProgramID, 
-(chl.amount*vincentp.commission) AS VaccinatorIncentive,  
-chl.lotteryDate, 
-chl.hasWonLottery, 
-chl.code, 
-chl.codeStatus, 
+vctor.identifier AS vaccinatorId, 
+vcen.identifier centerId,
+chl.incentiveDate, 
+chl.incentiveStatus, 
 chl.consumptionDate,  
 chl.transactionDate, 
-chl.amount, 
-stk.identifier storekeeperid  
-FROM childlottery chl 
+chl.amount 
+FROM childincentive chl 
 left join vaccination vtn on chl.vaccinationRecordNum = vtn.vaccinationRecordNum 
 left join idmapper id on vtn.childid=id.mappedid 
 LEFT JOIN identifier i ON i.mappedId=id.mappedId AND i.preferred=TRUE 
 left join vaccine vc on vtn.vaccineid=vc.vaccineid 
-left join idmapper stkidm on chl.storekeeperId=stkidm.mappedid 
-LEFT JOIN identifier stk ON stkidm.mappedId=stk.mappedId AND stk.preferred=TRUE 
-left join idmapper vctoridm on vtn.vaccinatorId=vctoridm.mappedid 
-LEFT JOIN identifier vctor ON vctoridm.mappedId=vctor.mappedId AND vctor.preferred=TRUE 
+LEFT JOIN identifier vctor ON vtn.vaccinatorId=vctor.mappedId AND vctor.preferred=TRUE 
+LEFT JOIN identifier vcen ON vcen.mappedId=vtn.vaccinationCenterId AND vcen.preferred=TRUE 
 left join child ch on vtn.childid=ch.mappedid 
-left join (select * from vaccinatorincentiveparams) vincentp on vincentp.commission is not null 
 left join vaccine chvc on ch.enrollmentVaccineId=chvc.vaccineId 
